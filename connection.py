f8ba3d69cca9e97473f49b09f3147b3abc983165
Dinesh Keserla <50540419+dkeserla@users.noreply.github.com>
2019-11-09 22:23:47 -0600

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,6 +1,6 @@
 import requests
 
-#class returns html data from the site
+#method that initializes the connection to HomeAccessCenter and returns the contained HTML in the page
 def connect(user_name, pass_word):
     header = {
         'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
@@ -13,10 +13,12 @@ def connect(user_name, pass_word):
         'LogOnDetails.Password' : pass_word
     }
 
-    #method utilizes get and post requests from the website in order to get the HTML associated with the webpage
+    #creates a session that initiates a login connection with specified paramaters
     with requests.Session() as c:
         url = 'https://lis-hac.eschoolplus.powerschool.com/HomeAccess/Account/LogOn?ReturnUrl=%2fHomeAccess'
         c.get(url)
         c.post(url, data = login_data, headers = header)
         page = c.get('https://lis-hac.eschoolplus.powerschool.com/HomeAccess/Home/WeekView')
         return page
+
+
