2fd2b307a5ec2378e5542baaa10800d202b1d324
Atul Errabolu <54562054+atulerrabolu@users.noreply.github.com>
2019-11-09 22:22:16 -0600

    Added documentation to Connection class

--- connection.py
+++ connection.py
@@ -1,5 +1,6 @@
 import requests
 
+#class returns html data from the site
 def connect(user_name, pass_word):
     header = {
         'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
@@ -12,6 +13,7 @@ def connect(user_name, pass_word):
         'LogOnDetails.Password' : pass_word
     }
 
+    #method utilizes get and post requests from the website in order to get the HTML associated with the webpage
     with requests.Session() as c:
         url = 'https://lis-hac.eschoolplus.powerschool.com/HomeAccess/Account/LogOn?ReturnUrl=%2fHomeAccess'
         c.get(url)
