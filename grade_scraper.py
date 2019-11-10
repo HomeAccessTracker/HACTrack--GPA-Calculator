4e0d2b6eaa1e37b72b7bfac192da1ef649c9bb02
Dinesh Keserla <50540419+dkeserla@users.noreply.github.com>
2019-11-09 22:14:46 -0600

    Update grade_scraper.py

--- grade_scraper.py
+++ grade_scraper.py
@@ -22,6 +22,22 @@ for grade in soup.find_all(attrs={'id': 'average'}):
 
 for course in range(len(all_courses)):
     courses[all_courses[course]] = all_grades[course]
+    
+weighted_total = 0
+total_minus = 0
+print(courses)
+for course in courses:
 
+    if course in weighted_classes or 'AP' in course:
+        weighted_total += 1
+    else:
+        courses[course] -= 10
+
+    total_minus += (100-courses[course])*.1
+    print(total_minus)
+    
 print(all_courses)
 print(all_grades)
+
+GPA = ((5*(len(courses)-weighted_total) + 6*weighted_total)-total_minus)/len(courses)
+print(round(GPA, 3)) 
